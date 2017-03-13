021e5a25cddadb90a6b3549a007e2225496545e8
Seshpenguin <seshan10@me.com>
2017-03-13 10:35:01 -0400

    Backend Framework

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 /*
- * EstiSite Backend
- * Make EstiSite dynamic/realtime again!(tm)(not really tm)
+ * EstiSite Server
+ * Serves and handles the website.
+ * Built on Express and Socket.IO
  * (c) 2017 DolphinBox
  */
-console.log("**Starting EstiSite backend server...**");
+console.log("[INFO] **Starting EstiSite backend server...**");
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('public'))
+
+app.use(function(req, res, next){
+    console.log('[ERROR] Client error 404');
+    res.sendFile(__dirname + '/public/errors/404.html');
+});
+
+
+//Socket.io
+io.on('connection', function(socket){
+    console.log('[INFO] New Socket.io client connection.');
+});
+
+server.listen(3000);
+
+
+console.log('[INFO] EstiSite Server listening on port 3000.')
